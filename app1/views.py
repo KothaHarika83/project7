35f4cc1d667d578c4a5f35e5a2754c54f811e200
kothaharika83 <harikakotha83@gmail.com>
2023-11-16 20:07:06 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def harika(request):
+    return HttpResponse('Harika industries')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def joshna(request):
+    return HttpResponse('joshna is the sister of harika')
